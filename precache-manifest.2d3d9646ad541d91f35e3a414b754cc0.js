e7715f611d64595eeca087c039790db23a0d935c
StanislavTemelkov <112681240+StanislavTemelkov@users.noreply.github.com>
2024-04-20 14:26:38 +0300

    Updates

---
+++ precache-manifest.2d3d9646ad541d91f35e3a414b754cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7753233554cbc0e0caf34628d6cb08e4",
+    "url": "/TEMELKOV.IS-A.DEV/index.html"
+  },
+  {
+    "revision": "6dda6eb946e853230bee",
+    "url": "/TEMELKOV.IS-A.DEV/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "745fb55b8ab8459500e3",
+    "url": "/TEMELKOV.IS-A.DEV/static/css/main.8911fe16.chunk.css"
+  },
+  {
+    "revision": "6dda6eb946e853230bee",
+    "url": "/TEMELKOV.IS-A.DEV/static/js/2.e12eb6d2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/TEMELKOV.IS-A.DEV/static/js/2.e12eb6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745fb55b8ab8459500e3",
+    "url": "/TEMELKOV.IS-A.DEV/static/js/main.392d5fa7.chunk.js"
+  },
+  {
+    "revision": "f1fade5ecaa8ad8055ea",
+    "url": "/TEMELKOV.IS-A.DEV/static/js/runtime-main.4a3cb0e0.js"
+  }
+]);
